8760aee68b2262741766d7f9d53649be819b2533
Dylan Meeus <meeusdylan@protonmail.com>
2021-01-19 13:48:04 +0100

    Add Chapter6 code

--- Chapter6/main.scala
+++ Chapter6/main.scala
@@ -37,3 +37,26 @@ val selectedColumns = simpleColors.map(c => {
 }):+expr("*")
 
 df.select(selectedColumns:_*).where($"is_white").select("Description").show(3)
+
+
+// working with dates and times
+val dateDF = spark.range(10).withColumn("today", current_date()).withColumn("now", current_timestamp())
+dateDF.createOrReplaceTempView("dateTable")
+
+dateDF.select(to_date($"today", "dd-mm-yyyy")).show(1)
+
+// working with structs
+val complexDF = df.select(struct("Description", "InvoiceNo").alias("complex"))
+complexDF.select($"complex".getField("Description"))
+
+// splits and explosions
+df.withColumn("splitted", split($"Description", " ")).
+  withColumn("exploded", explode($"splitted")).select("Description", "InvoiceNo", "exploded").show(4)
+
+// working with json
+val jsonDF = spark.range(1).selectExpr("""
+  '{"myKey": {"myVal": [1,2,3]}}' as jsonString""")
+
+jsonDF.select(
+  get_json_object($"jsonString", "$.myKey.myVal[0]") as "column", json_tuple($"jsonString", "myKey")).show(2)
+
